cc604d4bf82d348f573bdbb5079f002b2c4559fc
keevbush <keevbush@gmail.com>
2024-04-02 22:26:34 +0200

    Ajoutez des fichiers projet.

---
+++ DeezerMusicSync.Native/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DeezerMusicSync.Native
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
